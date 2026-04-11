78d1b894cc18758a8f804c1b692cb91a09943dd2
ayushshh <bs9882074@gmail.com>
2026-04-11 09:19:56 +0000

    finalizing clerk authentication

---
+++ modules/auth/actions/index.ts
@@ -0,0 +1,46 @@
+"use server"
+import { db } from "@/lib/db"
+import { currentUser } from "@clerk/nextjs/server"
+
+export const onBoardUser = async() => {
+    try {
+        const user = await currentUser();
+        if(!user){
+            return {
+                success: false,
+                error: "No authenticated user found"
+            }
+        }
+        const { id, firstName, lastName, imageUrl, emailAddresses } = user;
+        const newUser = await db.user.upsert({
+            where:{
+                clerkId: id
+            },
+            update:{
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses[0].emailAddress || ""
+            },
+            create:{
+                clerkId: id,
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses[0].emailAddress || ""
+            }
+        })
+
+        return {
+            success: true,
+            data: newUser,
+            message: "User onBoarded successfully"
+        }
+    } catch (error) {
+        console.error("❌ Error onboarding user:", error);
+        return {
+            success: false,
+            error: "Error onboarding user"
+        }
+    }
+}
